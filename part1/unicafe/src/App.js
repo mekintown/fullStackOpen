c0092688a67c02a7238d2f2ae18e94d3058aa3a2
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-03 14:38:59 +0700

    Finish 1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,9 +6,9 @@ const Button = ({ name, handleClick }) => (
     <button onClick={handleClick}>{name}</button>
 );
 
-const Content = ({ name, count }) => (
+const Statistics = ({ name, value, unit }) => (
     <p>
-        {name} {count}
+        {name} {value} {unit}
     </p>
 );
 
@@ -18,6 +18,21 @@ const App = () => {
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
+    if (good === 0 && neutral === 0 && bad === 0) {
+        return (
+            <div>
+                <Header title="give feedback" />
+                <Button name="good" handleClick={() => setGood(good + 1)} />
+                <Button
+                    name="neutral"
+                    handleClick={() => setNeutral(neutral + 1)}
+                />
+                <Button name="bad" handleClick={() => setBad(bad + 1)} />
+                <Header title="statistics" />
+                <p>No feedback given</p>
+            </div>
+        );
+    }
     return (
         <div>
             <Header title="give feedback" />
@@ -28,17 +43,18 @@ const App = () => {
             />
             <Button name="bad" handleClick={() => setBad(bad + 1)} />
             <Header title="statistics" />
-            <Content name="good" count={good} />
-            <Content name="neutral" count={neutral} />
-            <Content name="bad" count={bad} />
-            <Content name="all" count={good + neutral + bad} />
-            <Content
+            <Statistics name="good" value={good} />
+            <Statistics name="neutral" value={neutral} />
+            <Statistics name="bad" value={bad} />
+            <Statistics name="all" value={good + neutral + bad} />
+            <Statistics
                 name="average"
-                count={(good * 1 + bad * -1) / (good + neutral + bad)}
+                value={(good * 1 + bad * -1) / (good + neutral + bad)}
             />
-            <Content
+            <Statistics
                 name="positive"
-                count={(good / (good + neutral + bad)) * 100}
+                value={(good / (good + neutral + bad)) * 100}
+                unit="%"
             />
         </div>
     );
