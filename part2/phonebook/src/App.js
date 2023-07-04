36da4f340841783e53168bd812333baba5822722
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-04 16:45:21 +0700

    Fix useEffect to only run along with the first render

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -100,7 +100,7 @@ const App = () => {
         axios
             .get("http://localhost:3001/persons")
             .then((response) => setPersons(response.data));
-    });
+    }, []);
     return (
         <div>
             <h2>Phonebook</h2>
