5bb6f4608f27b2922853196ef86e30e4d3b38a7f
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-10 13:24:12 +0700

    Finish 4.9: Blog list tests, step2

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const helper = require("./test_helper");
+const Blog = require("../models/blog");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+    const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+    const promiseArray = blogObjects.map((blog) => blog.save());
+    await Promise.all(promiseArray);
+});
 
 test("blogs are returned as json", async () => {
     await api
@@ -11,6 +20,11 @@ test("blogs are returned as json", async () => {
         .expect("Content-Type", /application\/json/);
 });
 
+test("unique identifier property of the blog posts is named id", async () => {
+    const blogs = await api.get("/api/blogs");
+    expect(blogs.body[0].id).toBeDefined();
+    expect(blogs.body[0]._id).toBeUndefined();
+});
 afterAll(async () => {
     await mongoose.connection.close();
 });
