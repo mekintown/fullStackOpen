35cc043ded5bf362c43b4a5cde5dc147504c755a
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-10 10:10:22 +0700

    Finish 4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => 1;
+
 const totalLikes = (blogs) =>
     blogs.reduce((accumulator, { likes }) => accumulator + likes, 0);
+
+const favoriteBlog = (blogs) =>
+    blogs.reduce((max, blog) => (max.likes > blog.likes ? max : blog));
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
