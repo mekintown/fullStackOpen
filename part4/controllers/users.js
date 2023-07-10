a9d77d3dfe71c07ed594c4cd5c50acac596106f9
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-10 16:49:27 +0700

    Finish 4.16*: bloglist expansion, step4

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -2,17 +2,27 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
-usersRouter.post("/", async (request, response) => {
+usersRouter.post("/", async (request, response, next) => {
     const { username, name, password } = request.body;
+    if (password.length < 3) {
+        const error = new Error("Password must be at least 3 characters long.");
+        error.name = "ValidationError";
+        next(error);
+    }
 
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    const user = new User(username, name, passwordHash);
+    const user = new User({ username, name, passwordHash });
 
     const savedUser = await user.save();
 
     response.status(201).json(savedUser);
+});
+
+usersRouter.get("/", async (request, response) => {
+    const users = await User.find({});
+    response.json(users);
 });
 
 module.exports = usersRouter;
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const bcrypt = require("bcrypt");
-const { beforeEach } = require("node:test");
 const app = require("../app");
 const helper = require("./test_helper");
 const Blog = require("../models/blog");
@@ -133,7 +132,7 @@ describe("when there is initially one user in db", () => {
         const userAtStart = await helper.usersInDb();
 
         const newUser = {
-            username: "mluukkai",
+            username: "mluxaxsxukkai",
             name: "Matti Luukkainen",
             password: "salainen",
         };
@@ -149,6 +148,69 @@ describe("when there is initially one user in db", () => {
 
         const usernames = userAtEnd.map((user) => user.username);
         expect(usernames).toContain(newUser.username);
+    });
+
+    test("creation fails with proper statuscode and message if username already taken", async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        const newUser = {
+            username: "root",
+            name: "Superuser",
+            password: "salainen",
+        };
+
+        const result = await api
+            .post("/api/users")
+            .send(newUser)
+            .expect(400)
+            .expect("Content-Type", /application\/json/);
+
+        expect(result.body.error).toContain("expected `username` to be unique");
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toEqual(usersAtStart);
+    });
+
+    test("creation fails with proper statuscode and message if password is too short", async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        const newUser = {
+            username: "root",
+            name: "Superuser",
+            password: "sa",
+        };
+
+        const result = await api
+            .post("/api/users")
+            .send(newUser)
+            .expect(400)
+            .expect("Content-Type", /application\/json/);
+
+        expect(result.body.error).toContain("3");
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toEqual(usersAtStart);
+    });
+
+    test("creation fails with proper statuscode and message if username is too short", async () => {
+        const usersAtStart = await helper.usersInDb();
+
+        const newUser = {
+            username: "r",
+            name: "Superuser",
+            password: "s312312a",
+        };
+
+        const result = await api
+            .post("/api/users")
+            .send(newUser)
+            .expect(400)
+            .expect("Content-Type", /application\/json/);
+
+        expect(result.body.error).toContain("length");
+
+        const usersAtEnd = await helper.usersInDb();
+        expect(usersAtEnd).toEqual(usersAtStart);
     });
 });
 
