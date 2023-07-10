4937bf53d437f762965fccffcde2eb7e156f0b81
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-10 13:33:48 +0700

    Finish 4.10: Blog list tests, step3

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -25,6 +25,29 @@ test("unique identifier property of the blog posts is named id", async () => {
     expect(blogs.body[0].id).toBeDefined();
     expect(blogs.body[0]._id).toBeUndefined();
 });
+
+test("making an HTTP POST request to the /api/blogs URL successfully creates a new blog post", async () => {
+    const blogObject = {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0,
+    };
+    await api
+        .post("/api/blogs")
+        .send(blogObject)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((blog) => blog.title);
+    expect(titles).toContain("Canonical string reduction");
+});
+
 afterAll(async () => {
     await mongoose.connection.close();
 });
