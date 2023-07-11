518640ed6f95baa3e6b0bd1e8a4ba5fa1925c18b
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-11 09:53:26 +0700

    Finish 4.18: bloglist expansion, step6

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
