59b7dd539a7197f230b5feabdf733b46a1dc7baa
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-14 16:20:21 +0700

    Finish 6.9 Better anecdotes, step7

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { vote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-	const anecdotes = useSelector((state) => state);
+	const anecdotes = useSelector((state) =>
+		state.filter
+			? state.anecdotes.filter((anecdote) =>
+					anecdote.content.includes(state.filter)
+			  )
+			: state.anecdotes
+	);
 	const dispatch = useDispatch();
 
 	return (
