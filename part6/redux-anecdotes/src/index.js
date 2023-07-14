0ba4d2070e25106c5c1fe6e9882d898803328ecf
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-14 17:48:30 +0700

    Finish 6.14 Anecdotes and the backend, step1

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import anecdoteReducer from "./reducers/anecdoteReducer";
-import filterReducer from "./reducers/filterReducer";
 import store from "./store";
+import { setAnecdote } from "./reducers/anecdoteReducer";
+import anecdoteService from "./services/anecdote";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
 		<App />
 	</Provider>
 );
+
+anecdoteService
+	.getAll()
+	.then((anecdotes) => store.dispatch(setAnecdote(anecdotes)));
