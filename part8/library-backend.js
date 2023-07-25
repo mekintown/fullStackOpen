79417a81835ff1f4a996dd95a5dabeb6bae71e8d
Maekin <74455990+mekintown@users.noreply.github.com>
2023-07-25 23:25:54 +0700

    Add 8.5: Books by genre

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -114,7 +114,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
 	authorCount: Int!
-	allBooks(author: String): [Book!]!
+	allBooks(author: String, genre: String): [Book!]!
 	allAuthors: [Author!]!
   }
 `;
@@ -124,10 +124,17 @@ const resolvers = {
 		bookCount: () => books.length,
 		authorCount: () => authors.length,
 		allBooks: (root, args) => {
-			if (!args.author) {
-				return books;
+			let filterBook = books;
+
+			if (args.author) {
+				filterBook = filterBook.filter((book) => book.author === args.author);
 			}
-			return books.filter((book) => book.author === args.author);
+			if (args.genre) {
+				filterBook = filterBook.filter((book) =>
+					book.genres.includes(args.genre)
+				);
+			}
+			return filterBook;
 		},
 		allAuthors: () => {
 			return authors;
