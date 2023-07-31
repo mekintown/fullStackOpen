baef305d06b023673855f0caa775c23480df7f1e
Maekin <74455990+mekintown@users.noreply.github.com>
2023-08-01 02:08:37 +0700

    Add 8.19 Books by genre, part 1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,16 +1,38 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import { useEffect, useState } from "react";
 
 const Books = (props) => {
-	const result = useQuery(ALL_BOOKS);
+	const [selectedGenre, setSelectedGenre] = useState(null);
+	const [fetchedGenres, setFetchedGenres] = useState([]);
+
+	const result = useQuery(ALL_BOOKS, {
+		variables: { genre: selectedGenre },
+	});
+
+	useEffect(() => {
+		result.refetch();
+	}, [selectedGenre]);
+
 	if (!props.show) {
 		return null;
 	}
+
 	if (result.loading) {
 		return <div>loading...</div>;
 	}
 
 	const books = result.data.allBooks;
+
+	if (fetchedGenres.length === 0) {
+		const genres = books.map((book) => book.genres);
+		const uniqueGenres = [...new Set(genres.flat())];
+		setFetchedGenres(uniqueGenres);
+	}
+
+	const handleGenreClick = ({ target }) => {
+		setSelectedGenre(target.value);
+	};
 
 	return (
 		<div>
@@ -32,6 +54,14 @@ const Books = (props) => {
 					))}
 				</tbody>
 			</table>
+			{fetchedGenres.map((genre) => (
+				<button value={genre} key={genre} onClick={handleGenreClick}>
+					{genre}
+				</button>
+			))}
+			<button value={null} onClick={handleGenreClick}>
+				all genres
+			</button>
 		</div>
 	);
 };
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -12,12 +12,13 @@ export const ALL_AUTHORS = gql`
 `;
 
 export const ALL_BOOKS = gql`
-	query {
-		allBooks {
+	query allBooks($genre: String) {
+		allBooks(genre: $genre) {
 			title
 			author {
 				name
 			}
+			genres
 			published
 		}
 	}
