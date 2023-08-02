daf133c41f32e62b61b99820247bee5784f15c01
Maekin <74455990+mekintown@users.noreply.github.com>
2023-08-02 15:00:50 +0700

    Add 9.6 Eslint

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -6,71 +6,71 @@ const { userExtractor } = require("../utils/middleware");
 require("express-async-errors");
 
 blogRouter.get("/", async (request, response) => {
-    const blogs = await Blog.find({}).populate("user", {
-        username: 1,
-        name: 1,
-    });
-    response.json(blogs);
+	const blogs = await Blog.find({}).populate("user", {
+		username: 1,
+		name: 1,
+	});
+	response.json(blogs);
 });
 
 blogRouter.post("/", async (request, response) => {
-    let newBlog = request.body;
+	let newBlog = request.body;
 
-    const decodedToken = jwt.verify(request.token, process.env.SECRET);
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: "token invalid" });
-    }
-    if (!request.body.likes) {
-        newBlog = { ...request.body, likes: 0 };
-    }
+	const decodedToken = jwt.verify(request.token, process.env.SECRET);
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: "token invalid" });
+	}
+	if (!request.body.likes) {
+		newBlog = { ...request.body, likes: 0 };
+	}
 
-    const user = await User.findById(decodedToken.id);
+	const user = await User.findById(decodedToken.id);
 
-    const blog = new Blog({ ...newBlog, user: user.id });
+	const blog = new Blog({ ...newBlog, user: user.id });
 
-    const savedBlog = await blog.save();
-    user.blogs = user.blogs.concat(savedBlog._id);
-    await user.save();
+	const savedBlog = await blog.save();
+	user.blogs = user.blogs.concat(savedBlog._id);
+	await user.save();
 
-    const savedBlogPopulated = await Blog.findById(savedBlog._id).populate(
-        "user",
-        {
-            username: 1,
-            name: 1,
-        }
-    );
-    response.status(201).json(savedBlogPopulated);
+	const savedBlogPopulated = await Blog.findById(savedBlog._id).populate(
+		"user",
+		{
+			username: 1,
+			name: 1,
+		}
+	);
+	response.status(201).json(savedBlogPopulated);
 });
 
 blogRouter.delete("/:id", userExtractor, async (request, response) => {
-    const decodedToken = jwt.verify(request.token, process.env.SECRET);
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: "token invalid" });
-    }
+	const decodedToken = jwt.verify(request.token, process.env.SECRET);
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: "token invalid" });
+	}
 
-    if (request.user !== decodedToken.id) {
-        return response.status(401).json({ error: "invalid user" });
-    }
-    await Blog.findByIdAndRemove(request.params.id);
-    response.status(204).end();
+	if (request.user !== decodedToken.id) {
+		return response.status(401).json({ error: "invalid user" });
+	}
+	await Blog.findByIdAndRemove(request.params.id);
+	response.status(204).end();
 });
 
 blogRouter.put("/:id", async (request, response) => {
-    const { title, author, url, likes } = request.body;
+	const { title, author, url, likes } = request.body;
 
-    const blog = {
-        title,
-        author,
-        url,
-        likes,
-    };
+	const blog = {
+		title,
+		author,
+		url,
+		likes,
+	};
 
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
-        new: true,
-        runValidators: true,
-        context: "query",
-    });
-    response.json(updatedBlog);
+	const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+		new: true,
+		runValidators: true,
+		context: "query",
+	});
+	response.json(updatedBlog);
 });
 
 module.exports = blogRouter;
---
+++ part9/partb/bmiCalculator.ts
@@ -0,0 +1,48 @@
+interface calculateValues {
+  height: number;
+  weight: number;
+}
+
+export const parseArguments = (args: string[]): calculateValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+export const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height / 100) ** 2;
+
+  if (bmi < 16) {
+    return "Severely Underweight";
+  } else if (bmi >= 16 && bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal (Healthy Weight)";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight";
+  } else if (bmi >= 30 && bmi < 35) {
+    return "Obese Class I (Moderate)";
+  } else if (bmi >= 35 && bmi < 40) {
+    return "Obese Class II (Severe)";
+  } else {
+    return "Obese Class III (Very Severe or Morbidly Obese)";
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
