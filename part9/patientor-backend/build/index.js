fecaf4d3d6e0e042827c18182575f029ff230c7a
Maekin <74455990+mekintown@users.noreply.github.com>
2023-08-03 02:23:26 +0700

    Add 9.9: Patientor backend, step2

--- part9/patientor-backend/build/index.js
+++ part9/patientor-backend/build/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const cors = require("cors");
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get("/ping", (_req, res) => {
+app.use(cors());
+const PORT = 3001;
+app.get("/api/ping", (_req, res) => {
     console.log("someone pinged here");
     res.send("pong");
 });
